fc2a41df36b7d6cc1ec8a148d404d3fc1718b952
niezhiliang <1132263176@qq.com>
2018-07-30 14:32:50 +0800

    m

--- src/main/java/com/niezhiliang/weixin/login/controller/Index.java
+++ src/main/java/com/niezhiliang/weixin/login/controller/Index.java
@@ -1,12 +1,13 @@
 package com.niezhiliang.weixin.login.controller;
 
+import com.niezhiliang.weixin.login.utils.HttpUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+import java.util.logging.Logger;
 
 /**
  * @Author NieZhiLiang
@@ -15,6 +16,19 @@ import java.util.Enumeration;
 @Controller
 @RequestMapping(value = "/")
 public class Index {
+    private final static Logger logger = Logger.getLogger(Index.class.getName());
+    @Value("${wx.appid}")
+    private String appid;
+
+    @Value("${wx.secret}")
+    private String secret;
+
+    @Value("${wx.grant_type}")
+    private String grant_type;
+
+    @Value("${wx.request_url}")
+    private String request_url;
+
     @Autowired
     private HttpServletRequest request;
 
@@ -26,11 +40,23 @@ public class Index {
     @RequestMapping(value = "callback")
     @ResponseBody
     public String callBack() {
-        Enumeration enu=request.getParameterNames();
-        while(enu.hasMoreElements()){
-            String paraName=(String)enu.nextElement();
-            System.out.println(paraName+": "+request.getParameter(paraName));
+        String code = request.getParameter("code");
+        if (code != null) {
+            StringBuffer url = new StringBuffer();
+            url.append(request_url)
+                    .append("appid=")
+                    .append("&secret=")
+                    .append(secret)
+                    .append("&code=")
+                    .append(code)
+                    .append("&grant_type=")
+                    .append(grant_type);
+            logger.info(url.toString());
+            String result =
+                HttpUtil.getResult(url.toString());
+            return result;
         }
+
         return "success";
     }
 
